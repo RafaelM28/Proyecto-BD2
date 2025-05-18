66231957d9bedc72a601af3db73dad631a1ae655
Pedrolp37 <56178798+Pedrolp37@users.noreply.github.com>
2025-05-18 01:31:47 -0400

    locura

--- API/kw.py
+++ API/kw.py
@@ -18,17 +18,31 @@ ACCESS_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 if not all([BEARER_TOKEN, API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET]):
     st.error("❌ Error: Falta una o más credenciales en el archivo .env")
     st.stop()
+    
+    """ CREDENCIALES PEDRO UCAB
+    bearer_token="AAAAAAAAAAAAAAAAAAAAAIo71wEAAAAAG2C6I0BT4xUD2dnSAqVf9mPMmTQ%3DAxBaCtj2VInuN4GkUidy4FjgHO1c53BbrhoA1SeP2kKyTpSYO8",
+    consumer_key="CScF43Cp29qhWtLi8dHxK8IvD",
+    consumer_secret="5A49kX6tXFFxqzb4GCtaVmB8Pt5GNR3MCC8dl7SQ7FffqoxCTU",
+    access_token="1923903042828603392-IruX2ZsriOetZvf47d3Hc41TcDu7xR",
+    access_token_secret="1XdRd8yfSvPQgKdvI7GiV0RccIZwOqCldzhk8tryY8btN"
+    """
 
+    """bearer_token=BEARER_TOKEN,
+    consumer_key=API_KEY,
+    consumer_secret=API_SECRET,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_SECRET
+    """
 # Autenticación
 client = tweepy.Client(
-    bearer_token="AAAAAAAAAAAAAAAAAAAAAN7H0QEAAAAAybgv%2FPYBlXykkvxZC3TQFVAv2WQ%3Dhc41yEFtBphbQZ025R8tg1pAGSXdJoI7qJSwhW3FpgCykBTihF",
-    consumer_key="laZSmoXlGHHPMeEYZjiRSmJY2",
-    consumer_secret="TVq9BYmAZwTA11lURLDqEZPx2Lk3lAP9E7HDX995EA0x3owSqx",
-    access_token="739201544235683841-XG41v5IyOwvJRebjneiwhtKjiS1OU0b",
-    access_token_secret="4Ky6EX0YxbMvqrkuOWf0XlaYFLGvoRTm0IIKhoPgvHFD8"
+    bearer_token="AAAAAAAAAAAAAAAAAAAAAPco1AEAAAAA92B%2Bt3aTXPdiiF%2BVTkPMJnFrboI%3D5VoiNqB0mZsBWLFUZycUEm2B2VscN5DXnbYVEZ9WF5dy829E51",
+    consumer_key="yWiERIm2Ed9GWMbq8bg0UR3XY",
+    consumer_secret="NnaHT23ZmRUFPAEJHKSanU6kw2vsUYD2R3QlkhA3z2sBcvLjYX",
+    access_token="2872126068-TE7SgvSCHZQVFmiPNrPKIX9613ImbWtN0pZLc8A",
+    access_token_secret="KilJoV0V0tQbsQGt1J3ReUOSPg241GC3uQaQF897ZifPL"
 )
 
-# Conexión a MongoDB
+# Conexión a MongoDB 
 db_connector = MongoDBConnector()
 
 def transform_tweet_to_dict(tweet):
